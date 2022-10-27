fa7fbd8fa56b4ed3c1037afc245194d5d1cb5e59
Oussama Jarrousse <oussama@jarrousse.org>
2022-10-28 00:37:04 +0200

    cleaner cron_job logging to syslog

--- cron_job.sh
+++ cron_job.sh
@@ -13,4 +13,4 @@ readonly SCRIPT_NAME=$(basename $0)
 DOCKER_CERTBOT_OUTPUT="$(docker-compose -f /root/rocketchat/docker-compose.yaml run --rm certbot)"
 printf "$DOCKER_CERTBOT_OUTPUT" 
 docker-compose -f /root/rocketchat/docker-compose.yaml exec nginx nginx -s reload
-printf "$DOCKER_CERTBOT_OUTPUT" | logger -p user.notice -t $SCRIPT_NAME
+printf "$DOCKER_CERTBOT_OUTPUT" | tr -d '\r' | logger -p user.notice -t $SCRIPT_NAME
